5664222d05d3480cfd6d205d420b298de3701af6
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-24 21:18:49 +0530

    fix: minor bugs

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -4,9 +4,12 @@ import { Navbar } from '../../components/Navbar/Navbar';
 import { NotesCard } from '../../components/NotesCard/NotesCard';
 import { useCart } from '../../context/notes-context';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { EditNote } from '../../components/EditNote/EditNote';
+import { useEdit } from '../../context/edit-context';
  export const NotesPage = () => {
   const {noteData} = useCart();
   console.log(noteData.notes);
+   const { isEdit, setEdit } =useEdit()
 
   return (
     <>
@@ -15,7 +18,9 @@ import { Sidebar } from '../../components/Sidebar/Sidebar';
         <Sidebar />
         <main className="notes-container">
           <AddNote />
-          
+          { isEdit ? 
+          <EditNote/> : <></>
+         }
 
           <div className="notes-card-display">
             {noteData.notes.map((item) => (
