8a88bb1c5bae65255e8e4f8903117bc29799cdfb
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-24 21:17:24 +0530

    feat: implemented login and logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/auth-context';
+import { useCart } from '../../context/notes-context';
 
 export const Navbar = () => {
+  const {user,setUser} = useAuth();
+  const navigate = useNavigate();
+   const logoutHandler = () => {
+     setUser({ token: null });
+     navigate("/");
+     
+   };
   return (
     <>
       <div class="nav-container">
         <div class="brand-name">NotesApp</div>
-
-        <Link to="/login" >
-          <button class="login-button">Login</button>
-        </Link>
+        {user.token === null ? (
+          <Link to="/login">
+            <button class="login-button">Login</button>
+          </Link>
+        ) : (
+          <div onClick={logoutHandler}>
+            <button class="login-button">Logout</button>
+          </div>
+        )}
       </div>
     </>
   );
