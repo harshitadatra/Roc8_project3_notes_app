b2290d64607512a5586508c2d35100daf5f0e19d
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-24 20:59:56 +0530

    feat : added provider

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { AuthProvider } from "./context/auth-context";
 import { NotesProvider } from "./context/notes-context";
 import { ArchiveProvider } from "./context/archive-context";
 import { DeleteProvider } from "./context/delete-context";
+import { EditProvider } from "./context/edit-context";
 
 // Call make Server
 makeServer();
@@ -15,15 +16,17 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <DeleteProvider>
-        <ArchiveProvider>
-          <NotesProvider>
-            <AuthProvider>
-              <App />
-            </AuthProvider>
-          </NotesProvider>
-        </ArchiveProvider>
-      </DeleteProvider>
+      <EditProvider>
+        <DeleteProvider>
+          <ArchiveProvider>
+            <NotesProvider>
+              <AuthProvider>
+                <App />
+              </AuthProvider>
+            </NotesProvider>
+          </ArchiveProvider>
+        </DeleteProvider>
+      </EditProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
